4178b6c2312be73a1de4953b12dfbc1358c315b3
NAINSOO <68545724+NAINSOO@users.noreply.github.com>
2023-09-27 17:30:11 +0900

    Update main.py
    instead of multiprocessing, Increasing batch size is used to enhance image resolution to be inferred.

--- main.py
+++ main.py
@@ -12,21 +12,34 @@ from glob import glob
 import time
 import cv2
 import multiprocessing
-from huggingface_hub import from_pretrained_keras
 
-def infer(model, original_image):
-    image = keras.utils.img_to_array(original_image)
-    image = image.astype("float32") / 255.0
-    image = np.expand_dims(image, axis=0)
-    output = model.predict(image)
-    output_image = output[0] * 255.0
+def infer(model, images):
+    imgs = []
+    for image in images:
+        img = keras.utils.img_to_array(image)
+        img = img.astype("float32") / 255.0
+        img = np.expand_dims(img, axis=0)
+        imgs.append(img)
+    
+    image =  np.concatenate((imgs[0],imgs[1],imgs[2],imgs[3]),axis=0)
+    output = model.predict(image, batch_size=4)
+    print("------output-------------")
+    print(output.shape)
+    print(output)
+    output_image = output * 255.0
     output_image = output_image.clip(0, 255)
+    print(output_image.shape)
+    '''
     output_image = output_image.reshape(
-        (np.shape(output_image)[0], np.shape(output_image)[1], 3)
+        (4, np.shape(output_image)[1], np.shape(output_image)[2], 3)
     )
-    output_image = Image.fromarray(np.uint8(output_image))
-    original_image = Image.fromarray(np.uint8(original_image))
-    return output_image, np.uint8(output_image)
+    '''
+    output_images = []
+    for img in output_image:
+        image = Image.fromarray(np.uint8(img))
+        output_images.append(np.uint8(image))
+    #output_image = Image.fromarray(np.uint8(output_image))
+    return output_images
 
 def read_image(image_path):
     image = tf.io.read_file(image_path)
@@ -35,23 +48,14 @@ def read_image(image_path):
     image = tf.cast(image, dtype=tf.float32) / 255.0
     return image
 
-def multi_infer(model, original_image, image_part, n):
-    _, enhanced_image = infer(model, original_image)
-    image_part[i] = enhanced_image
 
 if __name__=='__main__':
     print("hello")
 
     desired_size = (600, 400)
-    #save_option = tf.saved_model.SaveOptions(experimental_io_device="/job:localhost")
 
-    #model = tf.keras.models.load_model('mirnet', compile=False, options=save_option)
-    #model = from_pretrained_keras("keras-io/lowlight-enhance-mirnet")
+    model = tf.keras.models.load_model('mirnet', compile=False)
 
-    models = [tf.keras.models.load_model('mirnet', compile=False) for _ in range(4)]
-    print("--------------------")
-    print(models)
-    print("--------------------")
 
     # 공공데이터 충청남도 천안시_교통정보 CCTV RTSP
     capture = cv2.VideoCapture('rtsp://210.99.70.120:1935/live/cctv002.stream') # 노트북의 경우 0, 외부 장치 번호가 1~n 까지 순차적으로 할당
@@ -75,17 +79,11 @@ if __name__=='__main__':
         original_image_part.append(frame[400:800, 0:600].copy())
         original_image_part.append(frame[0:400, 600:1200].copy())
         original_image_part.append(frame[400:800, 600:1200].copy())
-        procs = []
-        image_part = [0]*4
-        for i in range(4):
-            p = multiprocessing.Process(target= multi_infer, args=(models[i], original_image_part[i], image_part,i))
-            p.start()  
-            procs.append(p)
-            #_, image_part[i] = infer(models[i], original_image_part[i])
+        
+        image_part = infer(model, original_image_part)
         
 
-        for pro in procs:
-            pro.join()
+        
         vertical_img1 = cv2.vconcat([image_part[0], image_part[1]])
         vertical_img2 = cv2.vconcat([image_part[2], image_part[3]])
 
