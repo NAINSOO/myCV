d0adee86892564f56114390ddf1895bf59c417a3
NAINSOO <i12force@naver.com>
2023-10-16 17:57:02 +0900

    Update main.py
    결과물 속도감 개선

--- main.py
+++ main.py
@@ -14,68 +14,107 @@ import cv2
 import multiprocessing
 
 def homorphic_filter(img):
-    img_YUV = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)    
-    y = img_YUV[:,:,0]    
- 
-    rows = y.shape[0]    
-    cols = y.shape[1]
- 
-    ### illumination elements와 reflectance elements를 분리하기 위해 log를 취함
-    #imgLog = np.log1p(np.array(y, dtype='float') / 255) # y값을 0~1사이로 조정한 뒤 log(x+1)
- 
-    imgLog = tf.math.log1p(tf.constant(y,dtype='float')/255)
+    ### homomorphic filter는 gray scale image에 대해서 밖에 안 되므로
 
+    ### YUV color space로 converting한 뒤 Y에 대해 연산을 진행
+
+    img_YUV = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)    
+
+    y = img_YUV[:,:,0]    
+
+    
+
+    rows = y.shape[0]    
+
+    cols = y.shape[1]
+
+ 
+
+### illumination elements와 reflectance elements를 분리하기 위해 log를 취함
+
+    imgLog = np.log1p(np.array(y, dtype='float') / 255) # y값을 0~1사이로 조정한 뒤 log(x+1)
+
+    
 
     ### frequency를 이미지로 나타내면 4분면에 대칭적으로 나타나므로 
+
     ### 4분면 중 하나에 이미지를 대응시키기 위해 row와 column을 2배씩 늘려줌
+
     M = 2*rows + 1
+
     N = 2*cols + 1
- 
+
+    
+
     ### gaussian mask 생성 sigma = 10
+
     sigma = 10
-    
-    
-    (X, Y) = tf.meshgrid(tf.linspace(0, N-1, N), tf.linspace(0, M-1, M)) # 0~N-1(and M-1) 까지 1단위로 space를 만듬
-    Xc = tf.math.ceil(N/2) # 올림 연산
-    Yc = tf.math.ceil(M/2)
-    X = tf.cast(X,tf.double)
-    Y = tf.cast(Y,tf.double)
-    Xc = tf.cast(Xc, tf.double)
-    Yc = tf.cast(Yc, tf.double)
+
+    (X, Y) = np.meshgrid(np.linspace(0, N-1, N), np.linspace(0, M-1, M)) # 0~N-1(and M-1) 까지 1단위로 space를 만듬
+
+    Xc = np.ceil(N/2) # 올림 연산
+
+    Yc = np.ceil(M/2)
+
     gaussianNumerator = (X - Xc)**2 + (Y - Yc)**2 # 가우시안 분자 생성
- 
+
+    
+
     ### low pass filter와 high pass filter 생성
-    LPF = tf.math.exp(-gaussianNumerator / (2*sigma*sigma))
+
+    LPF = np.exp(-gaussianNumerator / (2*sigma*sigma))
+
     HPF = 1 - LPF
+
     
+
     ### LPF랑 HPF를 0이 가운데로 오도록iFFT함. 
+
     ### 사실 이 부분이 잘 이해가 안 가는데 plt로 이미지를 띄워보니 shuffling을 수행한 효과가 났음
+
     ### 에너지를 각 귀퉁이로 모아 줌
-    LPF_shift = tf.signal.ifftshift(tf.identity(LPF))
-    HPF_shift = tf.signal.ifftshift(tf.identity(HPF))
+
+    LPF_shift = np.fft.ifftshift(LPF.copy())
+
+    HPF_shift = np.fft.ifftshift(HPF.copy())
+
     
-    imgLog = tf.cast(imgLog, tf.complex64)
-    LPF_shift = tf.cast(LPF_shift, tf.complex64)
-    HPF_shift = tf.cast(HPF_shift, tf.complex64)
 
     ### Log를 씌운 이미지를 FFT해서 LPF와 HPF를 곱해 LF성분과 HF성분을 나눔
-    img_FFT = tf.signal.fft2d(tf.identity(imgLog))
-    img_LF = tf.math.real(tf.signal.ifft2d(tf.identity(img_FFT) * LPF_shift)) # low frequency 성분
-    img_HF = tf.math.real(tf.signal.ifft2d(tf.identity(img_FFT) * HPF_shift)) # high frequency 성분
+
+    img_FFT = np.fft.fft2(imgLog.copy(), (M, N))
+
+    img_LF = np.real(np.fft.ifft2(img_FFT.copy() * LPF_shift, (M, N))) # low frequency 성분
+
+    img_HF = np.real(np.fft.ifft2(img_FFT.copy() * HPF_shift, (M, N))) # high frequency 성분
+
     
+
     ### 각 LF, HF 성분에 scaling factor를 곱해주어 조명값과 반사값을 조절함
+
     gamma1 = 0.3
+
     gamma2 = 1.5
+
     img_adjusting = gamma1*img_LF[0:rows, 0:cols] + gamma2*img_HF[0:rows, 0:cols]
+
     
+
     ### 조정된 데이터를 이제 exp 연산을 통해 이미지로 만들어줌
-    img_exp = tf.math.expm1(img_adjusting) # exp(x) + 1
-    img_exp = (img_exp - tf.math.minimum(img_exp)) / (tf.math.maximum(img_exp) - tf.math.minimum(img_exp)) # 0~1사이로 정규화
+
+    img_exp = np.expm1(img_adjusting) # exp(x) + 1
+
+    img_exp = (img_exp - np.min(img_exp)) / (np.max(img_exp) - np.min(img_exp)) # 0~1사이로 정규화
+
     img_out = np.array(255*img_exp, dtype = 'uint8') # 255를 곱해서 intensity값을 만들어줌
+
     
+
     ### 마지막으로 YUV에서 Y space를 filtering된 이미지로 교체해주고 RGB space로 converting
+
     img_YUV[:,:,0] = img_out
-    result = cv2.cvtColor(img_YUV, cv2.COLOR_YUV2RGB)
+
+    result = cv2.cvtColor(img_YUV, cv2.COLOR_YUV2BGR)
     return result
 
 
@@ -169,7 +208,7 @@ if __name__=='__main__':
     model = mirnet(size=(256, 256))
 
     rtsp = "rtsp://210.99.70.120:1935/live/cctv001.stream"
-    # 공공데이터 충청남도 천안시_교통정보 CCTV RTSP
+    # 공공데이터 충청남도 천안시_교통정보 CCTV RTSP 샤나인코더
     capture = cv2.VideoCapture('testvideo2.mp4') # 노트북의 경우 0, 외부 장치 번호가 1~n 까지 순차적으로 할당
 
     # 카메라의 속성 설정 메서드 set
@@ -180,10 +219,13 @@ if __name__=='__main__':
     count = 0
 
     # while을 통해서 카메라에서 프레임을 지속적으로 받는다.
-    while cv2.waitKey(10) < 0:
+    while cv2.waitKey(33) < 0:
         # ret = 카메라 상태, 비정상이면 False
         # frame = 현재 시점의 프레임
-        ret, frame = capture.read()
+        ret, frame = None, None
+        for _ in range(3):
+            time.sleep(0.01)
+            ret, frame = capture.read()
         # flip : flipcode 가 0 이면 가로대칭 변경. 1이면 세로대칭 변경 
        
         rst_img = model.infer(frame) 
